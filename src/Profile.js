e68afd6f8a925fb93e3be0a145b07d60bbee4fc5
Max Becker <max.becker@eng-its.de>
2020-10-06 14:42:03 +0200

    MUI-Styling: 'Profil'

--- src/Profile.js
+++ src/Profile.js
@@ -3,49 +3,88 @@ import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import * as UiStateSlice from "./redux/UiStateSlice";
 import "./App.css";
+// Material UI
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import TableContainer from '@material-ui/core/TableContainer';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
+import Box from '@material-ui/core/Box';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 function Profile(props) {
   const dispatch = useDispatch();
   let history = useHistory();
+  const classes = useStyles();
   const uiState = useSelector((state) =>
     UiStateSlice.selectUiState(state)
   );
 
-  function logout(){
+  function logout() {
     localStorage.removeItem('jwt');
     dispatch(UiStateSlice.setLoggedIn(false));
     history.push('/Login')
   }
 
   return (
-    <div className="month">
-      <div className="error">{uiState.currentError}</div>
-      <p>Benutzerprofil</p>
-      <div className="row">
-        <div className="col text-right"><ins>Name</ins>:</div>
-        <div className="col  text-left">{uiState.profile.name}</div>
+    <React.Fragment>
+      <CssBaseline />
+      <div className={classes.root}>
+        <Grid
+          container
+          spacing={1}
+          justify="center"
+          alignItems="center"
+        >
+          <Grid xs={12} item style={{ textAlign: 'center', marginBottom: '0.5em' }}>
+            <Typography variant="h6">Benutzerprofil</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Box style={{ marginLeft: '1em', marginRight: '1em' }}>
+              <List className={classes.root}>
+                <ListItem>
+                  <ListItemText primary="Name" secondary={uiState.profile.name} />
+                </ListItem>
+                <Divider />
+                <ListItem>
+                  <ListItemText primary="Benutzername" secondary={uiState.profile.username} />
+                </ListItem>
+                <Divider />
+                <ListItem>
+                  <ListItemText primary="Organisation" secondary={uiState.profile.organization} />
+                </ListItem>
+                <Divider />
+                <ListItem>
+                  <ListItemText primary="Role" secondary={uiState.profile.role} />
+                </ListItem>
+                <Divider />
+              </List>
+            </Box>
+          </Grid>
+          <Grid xs={12} item style={{ marginTop: '0.5em', textAlign: 'center' }}>
+            <Button variant="contained" onClick={() => logout()}>
+              Logout
+            </Button>
+          </Grid>
+        </Grid>
       </div>
-      <div className="row">
-        <div className="col text-right"><ins>Organisation</ins>:</div>
-        <div className="col text-left">{uiState.profile.organization}</div>
-      </div>
-      <div className="row">
-        <div className="col text-right"><ins>Benutzername</ins>:</div>
-        <div className="col text-left">{uiState.profile.username}</div>
-      </div>
-      <div className="row">
-        <div className="col text-right"><ins>Role</ins>:</div>
-        <div className="col text-left">{uiState.profile.role}</div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <button type="button" className="button" onClick={() => logout()}>
-            Logout
-          </button>
-        </div>
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
 
