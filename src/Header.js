274d3d2ba48b6fd8609639ade0a7ed8d6dc0e528
Max Becker <max.becker@eng-its.de>
2020-10-08 09:48:54 +0200

    MUI-Styling: 'Header'

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useLocation, Link } from "react-router-dom";
 import * as UiStateSlice from "./redux/UiStateSlice";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faUserSlash } from '@fortawesome/free-solid-svg-icons';
 import "./App.css";
+// Material UI
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import IconButton from "@material-ui/core/IconButton";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const dispatch = useDispatch();
   let history = useHistory();
   const loc = useLocation();
+  const classes = useStyles();
   const uiState = useSelector((state) =>
     UiStateSlice.selectUiState(state)
   );
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
 
   useEffect(() => {
     if (loc.pathname !== '/resetPassword') {
@@ -48,47 +68,52 @@ function Header(props) {
   }, [history, dispatch, uiState.loggedIn, loc.pathname])
 
   return (
-    <header className="sticky-top">
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="dropdown">
-          <button className="navbar-toggler" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            {uiState.loggedIn &&
-              <div>
-                <Link to="/TimeBooking"><li className="dropdown-item">Zeitbuchungen</li></Link>
-                <Link to="/Vacation"><li className="dropdown-item">Urlaub</li></Link>
-              </div>
-            }
-            <Link to="/Login">
-              <li className="dropdown-item">
-                {uiState.loggedIn ? 'Profil' : 'Login/Registrieung'}
-              </li>
-            </Link>
-            {uiState.loggedIn &&
-              uiState.profile.role === 'admin' &&
-              <div>
-                <hr />
-                <Link to="/Employees"><li className="dropdown-item">Mitarbeiter</li></Link>
-                <Link to="/VacationRequests"><li className="dropdown-item">Urlaubsanträge</li></Link>
-              </div>}
-
-          </ul>
-        </div>
-        <div>
-        </div>
-        <div>
-          <small>{uiState.loggedIn ? uiState.profile.name : ''}</small>
-          <Link to="/Login">
-            <button className="button">
+    <header>
+      <Box className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu" onClick={handleClick}>
+              <MenuIcon />
+            </IconButton>
+            <Menu id="simple-menu"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}>
+              {uiState.loggedIn &&
+                <span>
+                  <MenuItem component={Link} to="/TimeBooking" onClick={handleClose} style={{ textDecoration: 'none' }}>Zeitbuchungen</MenuItem>
+                  <MenuItem component={Link} to="/Vacation" onClick={handleClose}>Urlaub</MenuItem>
+                </span>
+              }
+              <MenuItem component={Link} to="/Login" onClick={handleClose}>{uiState.loggedIn ? 'Profil' : 'Login/Registrieung'}</MenuItem>
+              {uiState.loggedIn && uiState.profile.role === 'admin' &&
+                <span>
+                  <MenuItem component={Link} to="/Employees" onClick={handleClose}>Mitarbeiter</MenuItem>
+                  <MenuItem component={Link} to="/VacationRequests" onClick={handleClose}>Urlaubsanträge</MenuItem>
+                </span>
+              }
+            </Menu>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              TimesBook
+          </Typography>
+            <Typography variant="body2">{uiState.loggedIn ? uiState.profile.name : ''}</Typography>
+            <IconButton component={Link} to="/Login" color="inherit">
               <FontAwesomeIcon icon={uiState.loggedIn ? faUser : faUserSlash} />
-            </button>
-          </Link>
-        </div>
-      </nav>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </header>
   );
 }
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+}));
 
 export default Header;
