24ae4fad9faec5bc76d88f14f6f6b5e6df228fde
Max Becker <max.becker@eng-its.de>
2020-10-11 06:29:46 +0200

    Cleanup warnings

--- src/Login.js
+++ src/Login.js
@@ -11,7 +11,6 @@ import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
-import Box from "@material-ui/core/Box";
 
 function Login(props) {
   const [orga, setOrga] = useState("");
--- src/Vacation.js
+++ src/Vacation.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import * as UiStateSlice from "./redux/UiStateSlice";
 import "./App.css";
 //Material UI
 import Typography from '@material-ui/core/Typography';
@@ -8,9 +6,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Box from '@material-ui/core/Box';
 
 function Admin(props) {
-  // const uiState = useSelector((state) =>
-  //   UiStateSlice.selectUiState(state)
-  // );
 
   return (
     <React.Fragment>
