de6f17d7083a17b0b19f439a8ec2f5cbac419995
Max Becker <max.becker@eng-its.de>
2020-10-14 08:15:19 +0200

    Umstellung auf MUI + Fehlerbehandlung für errorCodes 4005, 4006

--- src/ResetPassword.js
+++ src/ResetPassword.js
@@ -3,33 +3,35 @@ import { useSelector, useDispatch } from "react-redux";
 import validate from "validate.js";
 import { useLocation, Link } from "react-router-dom";
 import * as UiStateSlice from "./redux/UiStateSlice";
-import "./App.css";
+// Material UI
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 function useQuery() {
-  return new URLSearchParams(useLocation().search)
+  return new URLSearchParams(useLocation().search);
 }
 
 function ResetPassword(props) {
   const query = useQuery();
-  const [pass, setPass] = useState('');
-  const [passRepeat, setPassRepeat] = useState('');
-  const [regKey] = useState(query.get('regKey'));
-  const [username] = useState(query.get('username'));
-  const [successMsg, setSuccessMsg] = useState('');
+  const [pass, setPass] = useState("");
+  const [passRepeat, setPassRepeat] = useState("");
+  const [regKey] = useState(query.get("regKey"));
+  const [username] = useState(query.get("username"));
+  const [successMsg, setSuccessMsg] = useState("");
   const [showLoginLink, setShowLoginLink] = useState(false);
-  const uiState = useSelector((state) =>
-    UiStateSlice.selectUiState(state)
-  );
+  const uiState = useSelector((state) => UiStateSlice.selectUiState(state));
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(UiStateSlice.setLoggedIn(false));
-    localStorage.removeItem('jwt')
+    localStorage.removeItem("jwt");
   }, [dispatch]);
 
   /**
- * Sets state for changed fields on tap event
- */
+   * Sets state for changed fields on tap event
+   */
   function handleChange(event) {
     switch (event.target.name) {
       case "pass":
@@ -60,73 +62,137 @@ function ResetPassword(props) {
     };
 
     // Validate input fields
-    const result = validate({ pass: pass, passRepeat: passRepeat }, constraints);
+    const result = validate(
+      { pass: pass, passRepeat: passRepeat },
+      constraints
+    );
     if (result !== undefined) {
       if (result.pass || result.passRepeat)
-        dispatch(UiStateSlice.setCurrentError('Passwort muss mind. 6 Zeichen lang sein'));
-    }
-    else
-      if (pass !== passRepeat)
-        dispatch(UiStateSlice.setCurrentError('Passwörter stimmen nicht überein'));
-      else {
-        fetch(`${process.env.REACT_APP_API_URL}/auth/setpass`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ username: username, password: pass, registrationKey: regKey }),
+        dispatch(
+          UiStateSlice.setCurrentError(
+            "Passwort muss mind. 6 Zeichen lang sein"
+          )
+        );
+    } else if (pass !== passRepeat)
+      dispatch(
+        UiStateSlice.setCurrentError("Passwörter stimmen nicht überein")
+      );
+    else {
+      fetch(`${process.env.REACT_APP_API_URL}/auth/setpass`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: pass,
+          registrationKey: regKey,
+        }),
+      })
+        .then(function (response) {
+          if (response.ok) {
+            setSuccessMsg("Das Passwort wurde erfolgreich gesetzt");
+            setShowLoginLink(true);
+            setTimeout(() => setSuccessMsg(""), 5000);
+          } else throw response;
         })
-          .then(function (response) {
-            if (response.ok) {
-              setSuccessMsg('Das Passwort wurde erfolgreich gesetzt');
-              setShowLoginLink(true);
-              setTimeout(() => setSuccessMsg(''), 5000);
-            }
-            else
-              throw response
-          })
-          .catch((err) => {
-            dispatch(UiStateSlice.setCurrentError('Das Setzen des Passwortes ist fehlgeschlagen.'));
-          });
-      }
-
-    setTimeout(() => dispatch(UiStateSlice.setCurrentError('')), 5000);
+        .catch((err) => {
+          err
+            .json()
+            .then((data) => {
+              if (data.errorCode === 4005)
+                dispatch(
+                  UiStateSlice.setCurrentError(
+                    "Das Passwort ist bereits gesetzt."
+                  )
+                );
+              else if (data.errorCode === 4006)
+                dispatch(
+                  UiStateSlice.setCurrentError(
+                    "Das Setzen des Passwortes ist nicht möglich. Falscher Registrierungsschlüssel."
+                  )
+                );
+            })
+            .catch((err) => {
+              console.log(err);
+              dispatch(
+                UiStateSlice.setCurrentError(
+                  "Das Setzen des Passwortes ist fehlgeschlagen."
+                )
+              );
+            });
+        });
+    }
+    setTimeout(() => dispatch(UiStateSlice.setCurrentError("")), 5000);
   }
 
   return (
-    <div>
-      <div className="error">{uiState.currentError}</div>
-      <div style={{ color: 'green' }}>{successMsg}</div>
-      { showLoginLink ?
-        <Link to="/Login">
-          <p>Login</p>
-        </Link>
-        :
-        <span>
-          <p>Passwort für '{query.get('username')}' setzen</p>
-          <div>Passwort</div>
-          <div>
-            <input
-              name="pass"
-              type="password"
-              value={pass}
-              onChange={handleChange}
-            />
-          </div>
-          <div>Passwort wiederholen</div>
-          <div>
-            <input
-              name="passRepeat"
-              type="password"
-              value={passRepeat}
-              onChange={handleChange}
-            />
-          </div>
-          <div style={{ marginBottom: '0.3em', marginTop: '0.3em' }}>
-            <button type="button" className="button" onClick={() => submitPass()}>Senden</button>
-          </div>
-        </span>}
-    </div>
+    <Grid
+      container
+      spacing={1}
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item>
+        <Typography variant="h6">
+          Passwort für '{query.get("username")}' setzen
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography style={{ color: "red", textAlign: "center" }}>
+          {uiState.currentError}
+        </Typography>
+        <Typography style={{ color: "green", textAlign: "center" }}>
+          {successMsg}
+        </Typography>
+      </Grid>
+      {showLoginLink ? (
+        <Grid item>
+          <Button component={Link} to="/Login">
+            Login
+          </Button>
+        </Grid>
+      ) : (
+        <Grid item>
+          <Grid
+            container
+            spacing={1}
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <TextField
+                id="pass"
+                type="password"
+                label="Passwort"
+                variant="outlined"
+                name="pass"
+                value={pass}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="passRepeat"
+                type="password"
+                label="Passwort wiederholen"
+                variant="outlined"
+                name="passRepeat"
+                value={passRepeat}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item style={{ marginBottom: "0.3em", marginTop: "0.3em" }}>
+              <Button variant="contained" onClick={() => submitPass()}>
+                Senden
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      )}
+    </Grid>
   );
 }
 
