400f01420372a60f3ebbca2a0a5ca625c8913521
Max Becker <max.becker@eng-its.de>
2020-10-21 14:00:58 +0200

    Fehlerbehandlung in 'Main' überarbeitet

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Switch, Route, useHistory, useLocation } from "react-router-dom";
 import * as UiStateSlice from "./redux/UiStateSlice";
@@ -16,9 +16,13 @@ import ConfirmAccount from "./ConfirmAccount";
 import Home from "./Home";
 import moment from "moment";
 import * as DateUtils from "./DateUtils";
+//Material UI
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
 function Main(props) {
   const uiState = useSelector((state) => UiStateSlice.selectUiState(state));
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   let history = useHistory();
   const loc = useLocation();
@@ -38,10 +42,12 @@ function Main(props) {
           }
         })
           .then((response) => {
-            if (response.ok)
-              return response.json();
+            if (response.status === 401) {
+              if (loc.pathname !== '/Login')
+                history.push('/Login');
+            }
             else
-              throw response
+              return response.json();
           })
           .then((json) => {
             dispatch(UiStateSlice.setProfile(json));
@@ -57,35 +63,36 @@ function Main(props) {
                 headers: { "auth-token": localStorage.getItem("jwt") },
               })
               .then((response) => {
-                if (response.ok) return response.json();
-                else throw response;
+                if (response.status === 401) {
+                  if (loc.pathname !== '/Login')
+                    history.push('/Login');
+                }
+                else
+                  return response.json();
               })
               .then((json) => dispatch(BookingEntriesSlice.setBookingEntries(json)))
-              .catch((error) => {
-                if (error.status === 401) {
-                  if(loc.pathname !== '/Login') 
-                    history.push('/Login');
-                } else
-                  dispatch(
-                    UiStateSlice.setCurrentError(
-                      "Fehler! Der Server antwortet nicht."
-                    )
-                  );
+              .catch((err) => {
+                console.log(err)
+                setError('Fehler! Der Server antwortet nicht.');
+
               });
           })
-          .catch((error) => {
-            if (error.status === 401) {
-              history.push('/Login');
-            }
-            else {
-              dispatch(UiStateSlice.setCurrentError('Fehler! Der Server antwortet nicht.'));
-            }
+          .catch((err) => {
+            console.log(err)
+            setError('Fehler! Der Server antwortet nicht.');
+
           });
+        setTimeout(() => setError(""), 5000);
       }
   }, [history, loc.pathname, dispatch, uiState.now]);
 
   return (
     <main>
+      <Box display="flex" justifyContent="center">
+        <Typography style={{ color: "red", textAlign: "center" }}>
+          {error}
+        </Typography>
+      </Box>
       <Switch>
         <Route path="/Home">
           <Home />
